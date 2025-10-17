83a107a05b75532b0dcca0d834ba47415a79fa15
caian23 <caian23@outlook.com>
2025-10-17 11:49:37 -0300

    build: buckets de state e versionamento do projeto

---
+++ Modulo-Terraform/projeto/var.tf
@@ -0,0 +1,11 @@
+variable "aws_profile" {
+  description = "Perfil da conta da AWS"
+  type        = string
+  default     = ""
+}
+
+variable "region" {
+  description = "Região"
+  type        = string
+  default     = "us-west-2"
+}
